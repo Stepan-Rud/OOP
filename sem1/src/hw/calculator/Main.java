31f807bc6088000d56cb432d5fc060f9f583cb64
Stepan-Rud <136429121+Stepan-Rud@users.noreply.github.com>
2023-11-22 18:06:13 +0700

    Update Main.java

--- sem1/src/hw/calculator/Main.java
+++ sem1/src/hw/calculator/Main.java
@@ -1,2 +1,12 @@
-package hw.calculator;public class Main {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc1 = new Calculator("12","5");
+        System.out.println(calc1.mult(Arrays.asList(calc1.getNum1(), calc1.getNum2())));
+        System.out.println(calc1.div(Arrays.asList(calc1.getNum1(), calc1.getNum2())));
+        System.out.println(calc1.div(Arrays.asList(1.2, 5.0, 2.1)));
+        System.out.println(calc1.div(Arrays.asList(1.3, 1.6, 2,5)));
+    }
 }
